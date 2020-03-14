cac49b85222ddf75079e5a785843a75e0ec998c1
zachary <astrozees@gmail.com>
2020-03-14 16:28:10 -0700

    Bump package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='luabins_py',
-    version='1.0.0',
+    version='1.1.0',
     packages=['luabins'],
     url='https://github.com/zsennenga/luabins_py',
     license='MIT License',
